aac34b705d8a56264b6cafb288a13d3c01ce6dc6
Bashkatov-spb <bashkatov.spb@mail.ru>
2023-08-24 15:39:33 +0300

    fix: remove readAndSolve function and changed description

--- runner.js
+++ runner.js
@@ -1,13 +1,1 @@
-// Подключить модуль работы с файловой системой.
-const fs = require('fs');
-
-// Подключить функцию readAndSolve из соответствующего файла.
-const readAndSolve = require('./readAndSolve');
-
-// Все судоку для решения доступны в файле puzzles.txt.
-// Прочесть файл puzzles.txt в кодировке 'utf-8' и передать его содержимое в функцию readAndSolve.
-fs.readFile(
-  './puzzles.txt',
-  'utf-8',
-  readAndSolve,
-);
+// Используйте для решения судоку необходимые функции из файла sudoku.js
